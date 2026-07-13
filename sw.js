268e0d696155d971e0abfa00dde16a43780c71d7
luanfelipecdeoliveira4034e-art <luanfelipecdeoliveira4034e@gmail.com>
2026-07-13 16:09:37 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'web-validade-v68'; // Atualizado para v68 para limpar o cache antigo e aplicar as correções
+const CACHE_NAME = 'web-validade-v69'; // Atualizado para v69 para limpar o cache antigo e aplicar as correções
 const urlsToCache = [
   './',
   './index.html',
